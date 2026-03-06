82d27c8af94131f8e5e9386b6ef7d258c15148c3
filipe99b <filipe99b@gmail.com>
2026-03-06 22:28:53 +0000

    Nível intermediário finalizado, colocando os cálculos de PIB per capita e densidade demográfica

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,19 +7,19 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
- //carta1
+    //carta1
     char estado1 [10] = "NA";
     char card_code1 [10] = "A01";
-    char cidade1 [30] = "NãoseAplica";
+    char cidade1 [30] = "NaoSeAplica";
     int populacao1 = 0, turismo1 = 0;
-    float area1 = 0, pib1 = 0;
+    float area1 = 0, pib1 = 0, pib_pc1 = 0, dens_pop1 = 0;
 
     //carta2
-    char estado2 [10] = "NB";
+    char estado2 [10] = "NA";
     char card_code2 [10] = "A02";
-    char cidade2 [30] = "NaoseAplica";
+    char cidade2 [30] = "NaoSeAplica";
     int populacao2 = 0, turismo2 = 0;
-    float area2 = 0, pib2 = 0;
+    float area2 = 0, pib2 = 0, pib_pc2 = 0, dens_pop2 = 0;
 
   // Área para entrada de dados
     printf("Olá! Seja bem vindo ao sistema de cadastramento de cartas do Super Trunfo!\n");
@@ -72,24 +72,35 @@ int main() {
     printf("Insira o PIB (Produto Interno Bruto): ");
     scanf("%f", &pib2);
 
+    //calculando pib per capita e densidade populacional
+    dens_pop1 = (float) populacao1 / area1;
+    dens_pop2 = (float) populacao2 / area2;
+    pib_pc1 = (float) (pib1 * 1000000000) / populacao1;
+    pib_pc2 = (pib2 * 1000000000) / populacao2;
+
   // Área para exibição dos dados da cidade
-    printf("\nCarta 1.\n");
-    printf("Estado: %s", estado1);
+    printf("\nCarta 1.");
+    printf("\nEstado: %s", estado1);
     printf("\nCódigo: %s", card_code1);
     printf("\nCidade: %s", cidade1);
     printf("\nPopulação: %i", populacao1);
     printf("\nNº de pontos turísticos: %i", turismo1);
     printf("\nÁrea: %.2f km²", area1);
-    printf("\nPIB: R$ %.2f BI\n", pib1);
+    printf("\nPIB: R$ %.2f BI", pib1);
+    printf("\nDensidade populacional: %.2f hab/km²", dens_pop1);
+    printf("\nPIB per capita: R$ %.2f\n", pib_pc1);
+    
 
-    printf("\nCarta 2.\n");
-    printf("Estado: %s", estado2);
+    printf("\nCarta 2.");
+    printf("\nEstado: %s", estado2);
     printf("\nCódigo: %s", card_code2);
     printf("\nCidade: %s", cidade2);
     printf("\nPopulação: %i", populacao2);
     printf("\nNº de pontos turísticos: %i", turismo2);
     printf("\nÁrea: %.2f km²", area2);
-    printf("\nPIB: R$ %.2f BI\n", pib2);
+    printf("\nPIB: R$ %.2f BI", pib2);
+    printf("\nDensidade populacional: %.2f hab/km²", dens_pop2);
+    printf("\nPIB per capita: R$ %.2f\n", pib_pc2);
 
 return 0;
 
